faf1c94fd6df5fa37c209973ba093d9b7d4bd338
Joram Kroon <joramkroon@live.nl>
2026-02-09 11:28:30 +0100

    add time helpers

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,29 @@
+export function formatDate(date: string | null) {
+	if (!date) return '';
+
+	return new Date(date).toLocaleDateString('en-US', {
+		month: 'long',
+		day: 'numeric',
+		year: 'numeric'
+	});
+}
+
+export function formatTime(date: string | null) {
+	if (!date) return '';
+
+	return new Date(date).toLocaleTimeString('en-US', {
+		hour: '2-digit',
+		minute: '2-digit'
+	});
+}
+
+export function formatDateTime(date: string | null) {
+	if (!date) return '';
+
+	return new Date(date).toLocaleString('en-US', {
+		month: 'long',
+		day: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit'
+	});
+}
