aa492be76afc9d472bcb575e975d4593e4dd59ef
Joram Kroon <joramkroon@live.nl>
2026-02-15 16:00:56 +0100

    create loader

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+	<p>LOADING</p>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		aspect-ratio: 1/1;
+	}
+
+	p {
+		font-size: 4rem;
+		animation: spin 500ms linear infinite;
+	}
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
